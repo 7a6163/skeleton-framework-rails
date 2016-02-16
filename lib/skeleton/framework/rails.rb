5e28ffa5109adc415cb3855fc7192ca19048be8a
Zac <mymengyu@gmail.com>
2016-02-16 13:38:38 +0800

    Ininital Commit

---
+++ lib/skeleton/framework/rails.rb
@@ -0,0 +1,10 @@
+require "skeleton/framework/rails/version"
+
+module Skeleton
+  module Framework
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/skeleton/framework/rails/version.rb
@@ -0,0 +1,7 @@
+module Skeleton
+  module Framework
+    module Rails
+      VERSION = "1.0.7"
+    end
+  end
+end
